8dae423ded2eb427b3c0e90637341cedfea5806a
Melvin <marchenko1985g@mail.ru>
2020-11-29 14:05:18 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,24 +6,40 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style type="text/css">
+    /*  анимированные переходы между компонентами */
+
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+</style>
 </head>
 
 <body>
-    
- 
-  
-<div id="app">
-<vue-layout></vue-layout>
-<router-view></router-view>
-<!-- <showuser-component></showuser-component> -->
-<!-- <input-form></input-form> -->
 
-        <!-- <v-header></v-header> -->
-        <!-- <router-view></router-view> -->
-        
-      
-</div>
-  
+
+
+    <div id="app">
+        <vue-layout></vue-layout>
+        <v-header></v-header>
+
+        <!-- анимированные переходы между компонентами -->
+        <transition name="component-fade" mode="out-in" >
+            <router-view></router-view>
+        </transition>
+        <!-- END -->
+
+        <!-- <showuser-component></showuser-component> -->
+        <!-- <input-form></input-form> -->
+
+
+    </div>
+
 
     <script type="application/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
